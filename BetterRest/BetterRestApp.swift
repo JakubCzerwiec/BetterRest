9390adf5f09ec6fa18407ac353d1731fc631f173
JakubCzerwiec <jacob.czerwiec@gmail.com>
2024-10-05 05:20:24 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mój Maczek on 05/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
